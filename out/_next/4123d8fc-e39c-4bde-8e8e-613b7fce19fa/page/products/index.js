4c143fcbafcd57350ed197b8f47e5c3d417dc699
Jake <jake@Jakes-MBP.lan>
2017-11-19 09:41:19 +0000

    products page rough

--- out/_next/4123d8fc-e39c-4bde-8e8e-613b7fce19fa/page/products/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/products', function() {
-            var comp = module.exports=webpackJsonp([5],{220:function(e,t,u){e.exports=u(221)},221:function(e,t,u){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n=u(1),l=function(e){return e&&e.__esModule?e:{default:e}}(n),r=function(){return l.default.createElement("div",null,l.default.createElement("h3",null,"Products"))};t.default=r}},[220]);
-            return { page: comp.default }
-          })
-        
--- pages/products.js
+++ pages/products.js
@@ -1,55 +1,195 @@
-import { Row, Col } from 'reactstrap'
-import ImageGallery from 'react-image-gallery'
-import Navbar from '../components/navbar'
+import React, { Component } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 import Layout from '../components/minimalLayout'
+import Header from '../components/header'
+import CartAddon from '../components/cartAddon'
 
-const Products = () => (
-  <Layout>
-    <div className="defaultHeader">
-      <Navbar />
-    </div>
-    <style jsx>{`
-      .defaultHeader {
-        background: #D7D7D7;
-        height: 116px;
-      }
-      .addToCart {
-        border-radius: 0px;
-        width: 417px;
-        height: 54px;
-        background: #0076e2;
-        box-shadow: 0px 2px 7px #0076E2;
-        color: #FFFFFF;
-        text-align: center;
-      }
-    `}
-    </style>
-    <Row>
-      <Col sm={6}>
-        <ImageGallery items={images} />
-      </Col>
-      <Col sm={6}>
-        <div className="addToCart mx-auto">
-          <span>ADD TO CART</span>
-        </div>
-      </Col>
-    </Row>
-  </Layout>
-)
-
-const images = [
+const items = [
   {
-    original: 'http://lorempixel.com/1000/600/nature/1/',
-    thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+    src: '/static/images/placeholderBig.png',
+    thumb: '/static/images/placeholderThumb.png',
+    altText: 'Slide 1',
+    caption: 'Slide 1'
   },
   {
-    original: 'http://lorempixel.com/1000/600/nature/2/',
-    thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+    src: '/static/images/placeholderBig.png',
+    thumb: '/static/images/placeholderThumb.png',
+    altText: 'Slide 1',
+    caption: 'Slide 1'
   },
   {
-    original: 'http://lorempixel.com/1000/600/nature/3/',
-    thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+    src: '/static/images/placeholderBig.png',
+    thumb: '/static/images/placeholderThumb.png',
+    altText: 'Slide 1',
+    caption: 'Slide 1'
   }
-]
+];
 
-export default Products
+class Example extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+          src={item.src}
+          altText={item.altText}
+        >
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <Layout>
+        <Container>
+          <Header color="secondary"/>
+          <Container>
+            <div className="productContent">
+              <Row>
+                <Col md={8}>
+                  <h2>Cove Protect</h2>
+                  <Carousel
+                    activeIndex={activeIndex}
+                    next={this.next}
+                    previous={this.previous}
+                  >
+                    <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                    {slides}
+                    <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+                    <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+                  </Carousel>
+                </Col>
+                <Col md={4} className="no-gutters">
+                  <div className="cartColumn">
+                    <div className="cartHeader">
+                      <h4>Cove Protect alarm system started pack</h4>
+                      <ul>
+                        <li>1 Cove Protect panel</li>
+                        <li>2 Cove doors</li>
+                        <li>1 Cove remote</li>
+                      </ul>
+                    </div>
+                    <div className="cartSubHeader">
+                      <h4>How many sensors should I add?</h4>
+                      <p>Don’t worry too much, you can add more sensors later, or send the ones you don’t need free of charge.</p>
+                    </div>
+                    <CartAddon />
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </Container>
+        <style jsx>{`
+          .productContent {
+            margin-top:60px;
+            width: 100%;
+            h2 {
+              margin-top: 55px;
+              margin-bottom: 35px;
+              font-family: GothamRoundedBold;
+              font-size: 40px;
+            }
+            ul {
+              list-style: none;
+              padding-left: 0px;
+            }
+          }
+          .cartHeader {
+            margin: 20px;
+          }
+          .cartSubHeader {
+            position: relative;
+            height: 142px;
+            margin-top:40px;
+            margin-bottom: 90px;
+            h4, p {
+              padding-left: 25px;
+              padding-right: 25px;
+            }
+            h4 {
+              padding-top: 0px;
+              color: #00B19A;
+            }
+            &::before {
+              content: "";
+              position: absolute;
+              width: 8px;
+              height: 100%;
+              margin-left: -1px;
+              border-top-right-radius: 4px;
+              border-bottom-right-radius: 4px;
+              background: #00B19A;
+            }
+          }
+          .cartColumn {
+            -webkit-box-shadow: 0px 6px 5px 2px rgba(44,50,57,0.2);
+            -moz-box-shadow: 0px 6px 5px 2px rgba(44,50,57,0.2);
+            box-shadow: 0px 6px 5px 2px rgba(44,50,57,0.2);
+            height: 1085px;
+            width: 385px;
+            margin-bottom: 15px;
+            border-radius: 16px;
+            border: 1px solid #F0F4F7;
+            h4 {
+              font-family: GothamRoundedBold;
+              font-size: 24px;
+            }
+          }
+        `}
+        </style>
+      </Layout>
+    );
+  }
+}
+
+
+export default Example;
