b9bad150a191c733640132bd55fc1d40f5506a25
jakegibson <jake@j3.io>
2017-12-06 22:59:42 +0000

    add coveclub route

--- pages/coveclub.js
+++ pages/coveclub.js
@@ -1,13 +1,21 @@
 import Link from 'next/link'
 import Layout from '../components/minimalLayout'
-import Header from '../components/homeHeader'
+import Header from '../components/header'
 import PriceComparisonTable from '../components/priceComparisonTable'
 import { Container, Row, Col } from 'reactstrap'
 
-const Index = () => (
+const CoveClub = () => (
   <Layout>
     <Container>
-      <Header color="secondary"/>
+      <Header color="secondary" />
+      <Row>
+        <Col>
+          <h2>Cove members to Cove Pay</h2>
+          <div className="titleSubText">
+            Save $5/mo on your monitoring and use CoveClub EasyPay to pay your equipment off over time with 0% APR
+          </div>
+        </Col>
+      </Row>
     </Container>
   </Layout>
 )
