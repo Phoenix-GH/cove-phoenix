75ca94c24b1df901af85c96ff03b4b0ec8d5494d
jakegibson <jake@j3.io>
2018-04-02 12:00:16 -0600

    add vwo snippet

--- components/minimalLayout.js
+++ components/minimalLayout.js
@@ -1,5 +1,15 @@
 import Head from 'next/head';
 
+const vwo = `<!-- Start Visual Website Optimizer Asynchronous Code -->
+var _vwo_code=(function(){
+var account_id=341440,
+settings_tolerance=2000,
+library_tolerance=2500,
+use_existing_jquery=false,
+/* DO NOT EDIT BELOW THIS LINE */
+f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();
+<!-- End Visual Website Optimizer Asynchronous Code -->`;
+
 export default props => (
   <div>
     <Head>
@@ -9,6 +19,7 @@ export default props => (
       <link rel="stylesheet" href="/static/css/default.css" />
       <link rel="stylesheet" href="/static/css/bundle.css" />
       <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400i,600,700" rel="stylesheet" />
+      <script type="text/javascript" dangerouslySetInnerHTML={{ __html: vwo }} />
     </Head>
     {props.children}
   </div>
