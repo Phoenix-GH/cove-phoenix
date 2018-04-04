b6cb4cf1d492715c320d03c76c5788911589b4bf
Marius Repede <facon3@protonmail.com>
2018-04-04 04:50:55 +0200

    Updates with redux-saga integration

--- sagas/index.js
+++ sagas/index.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose, reducer } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-
 import { rootSaga } from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(applyMiddleware(sagaMiddleware));
+
+const store = createStore(applyMiddleware(
+  reducer,
+  applyMiddleware(sagaMiddleware),
+));
 sagaMiddleware.run(rootSaga);
 
 const action = type => store.dispatch({ type });
--- sagas/sagas.js
+++ sagas/sagas.js
@@ -1,15 +1,18 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
+import axios from 'axios';
 
 function verifyContact() {
+  return axios({
+    method: 'get',
+    url: 'https://api.com',
+  });
 }
 function* workerSaga() {
   try {
     const response = yield call(verifyContact);
     const contact = response.data.message;
-    // dispatch a success action to the store with the new dog
     yield put({ type: 'API_CALL_SUCCESS', contact });
   } catch (error) {
-    // dispatch a failure action to the store with the error
     yield put({ type: 'API_CALL_FAILURE', error });
   }
 }
