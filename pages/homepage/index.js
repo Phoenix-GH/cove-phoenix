2cf1f08e3bc50f76d780e405e3cc3c40a4ec643d
Phoenix <phoenix214@protonmail.com>
2018-04-24 15:30:13 -0500

    Removed  unnecessary sections - Updates on stylesheets

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -2,11 +2,8 @@ import { Container, Row, Col } from 'reactstrap';
 import Link from 'next/link';
 import Layout from '../../components/minimalLayout';
 import Header from '../../components/header';
-import PriceComparisonTable from '../../components/priceComparisonTable';
 import Footer from '../../components/footer/footer';
-import WatchVideoLink from '../../components/watchVideoLink/watchVideoLink';
-import CustomizedRange from '../../components/estimateSlider';
-import RiskFreeSection from '../../components/riskFreeSection/riskFreeSection';
+
 import GreenButton from '../../components/greenButton/greenButton';
 import s from './index.scss';
 
@@ -176,74 +173,6 @@ const Index = () => (
           </Row>
         </div>
       </Row>
-      <Row>
-        <div className="eleganceSection">
-          <Row>
-            <Col xs={12} sm={12} md={8} lg={6}>
-              <div className="descriptionArea">
-                <div className="contentTextArea">
-                  <h2>New elegance and beauty. Same incredible protection.</h2>
-                  <p>Security systems shouldn&apos;t be made to be an eye sore for your home.
-                    We designed every element to be beautiful and seamless to fit perfectly
-                    into your home.
-                  </p>
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </Row>
-      <Row>
-        <div className="priceComparisonTableRow">
-          <PriceComparisonTable />
-        </div>
-      </Row>
-      <Row>
-        <div className="jordanSayingSection d-flex justify-content-center">
-          <div className="sayingTextArea">
-            <hr />
-            <p className="sayingText">&quot;Cove was so much less than competitors and
-            still maintains the quality I was used to.&quot;
-            </p>
-            <p className="authorName">- Jordan H.</p>
-            <hr />
-          </div>
-        </div>
-      </Row>
-      <Row>
-        <div className="savingsSection d-flex align-items-center">
-          <h2>Savings so big, you can&apos;t help but love it!</h2>
-          <p className="wideText">The average consumer saves every year by choosing Cove over other alarm companies.</p>
-          <p className="shortText">Every year with Cove is more money in your pocket</p>
-          <div className="rangeRow">
-            <CustomizedRange />
-          </div>
-        </div>
-      </Row>
-      <Row>
-        <div className="processSection">
-          <Row>
-            <Col xs={12} sm={12} md={8}>
-              <div className="leftArea">
-                <h2>The most simple setup process ever.</h2>
-                <p>Our unprecedented setup process makes it easy for everyone.</p>
-                <ol>
-                  <li>Plug in your panel.</li>
-                  <li>Follow on screen instructions.</li>
-                  <li>There is not step 3.</li>
-                </ol>
-                <WatchVideoLink link="/" size="small" title="Watch install video" />
-              </div>
-            </Col>
-            <Col xs={0} sm={0} md={4}>
-              <div className="rightArea">
-                <img src="/static/images/packageBox.png" alt="" />
-              </div>
-            </Col>
-          </Row>
-        </div>
-      </Row>
-      <RiskFreeSection />
     </div>
     <Footer />
     <style jsx>{s}</style>
