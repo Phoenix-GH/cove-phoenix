ec8152be1f5ba0115721bc19c43cd7fcf15ba90a
Stephen Tippets <stephen.tippets@gmail.com>
2018-04-26 17:31:10 -0600

    Style and link updates to footer

--- components/footerColumn/footerColumn.js
+++ components/footerColumn/footerColumn.js
@@ -1,29 +1,31 @@
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-import { Row } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import s from './footerColumn.scss';
 
 const FooterColumn = (props) => {
   const { title, list } = props;
   return (
-    <div className="footerColumn" style={{ width: props.width }}>
-      <Row>
-        <div className="leftbar" />
-        <div>
-          <h4>{title}</h4>
-          <ul>
-            {list.map(item => (
-              <li key={item.name}>
-                <Link href={item.link}>
-                  <a href={item.link}>{item.name}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </Row>
-      <style jsx>{s}</style>
-    </div>
+    <Col xs={12} sm={3}>
+      <div className="footerColumn">
+        <Row>
+          <div className="leftbar" />
+          <div>
+            <h4>{title}</h4>
+            <ul>
+              {list.map(item => (
+                <li key={item.name}>
+                  <Link href={item.link}>
+                    <a className="footerColumn__link" href={item.link}>{item.name}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Row>
+        <style jsx>{s}</style>
+      </div>
+    </Col>
   );
 };
 
