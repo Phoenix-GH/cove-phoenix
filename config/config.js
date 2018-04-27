a1dc372dd3d9a5f12352714d68cd6ac70ece0217
jakegibson <jake@j3.io>
2018-04-27 12:53:22 -0600

    add production api url config

--- config/config.js
+++ config/config.js
@@ -2,6 +2,7 @@ const env = process.env.NODE_ENV || 'development';
 const apiUrl = {
   development: 'https://2m1onye0xk.execute-api.us-east-1.amazonaws.com/dev',
   stage: 'https://x4266rjle9.execute-api.us-east-1.amazonaws.com/staging',
+  production: 'https://x4266rjle9.execute-api.us-east-1.amazonaws.com/staging',
 };
 
 console.log('env', env, apiUrl[env])
